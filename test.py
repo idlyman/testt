c134f5c9a4fd0525d495cd47f4db39e27cc975b7
idlyman <165511144+idlyman@users.noreply.github.com>
2025-09-16 12:03:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import time
 
 app = Flask(__name__)
 
@@ -6,7 +7,9 @@ app = Flask(__name__)
 def whoami():
     client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
     client_port = request.environ.get("REMOTE_PORT")  # Flask doesn't expose port directly
-    return {"ip": client_ip, "port": client_port}
+    print ("ip": client_ip, "port": client_port)
+    time.sleep(30)
+    return {"ip": client_ip, "port": client_port)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
