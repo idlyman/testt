19d5b1526f8eb21cbfb0c4ab797fab16e99a0cc7
idlyman <165511144+idlyman@users.noreply.github.com>
2025-09-16 12:04:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def whoami():
     client_port = request.environ.get("REMOTE_PORT")  # Flask doesn't expose port directly
     print ("ip": client_ip, "port": client_port)
     time.sleep(30)
-    return {"ip": client_ip, "port": client_port)
+    return {"ip": client_ip, "port": client_port}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
